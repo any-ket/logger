8de01e45e2a943bbb7b1ba1f2df7ba4147b8e7c4
Aniket Singh <aniketsingh84646@gmail.com>
2023-07-20 01:35:03 +0530

    TECH-73 Added logger for level wise logging with proper timestamps

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-const winston = require("winston");
+const winston = require('winston');
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, printf, colorize } = format;
 
-const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
+// Define custom log levels
+const customLevels = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    debug: 3,
+    silly: 4,
+  },
+  colors: {
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+    debug: 'cyan',
+    silly: 'magenta',
+  },
+};
+
+// Define the log format
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
 });
+
+// Create the logger instance with custom levels and colors
+const logger = createLogger({
+  levels: customLevels.levels,
+  format: combine(colorize(), timestamp(), logFormat),
+  transports: [
+    new transports.Console(),
+  ],
+});
+
+//setting to silly so that all above logs are shown
+logger.level = "silly";
+
+logger.log = (level, ...args) => {
+  args = args.map(arg => {
+    try{
+      if(typeof arg === 'object')
+        return JSON.stringify(arg);
+      return arg;
+    } catch(_error){
+      return arg;
+    }
+  });
+
+  const message = args.join(' ');
+  logger[level](message);
+};
 
 module.exports = logger;
